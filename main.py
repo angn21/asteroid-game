b8260075f1a12f153b6778ee1287d2aa67572e73
angn21 <anmolimportantstuff@gmail.com>
2026-01-18 13:24:02 +0700

    finalized version with splitting of asteroids

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ def main():
                 return
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+        for asteroid in asteroids:
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print ("Game over!")
